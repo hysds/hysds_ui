7b5683e434d056a274337f324876b65e2107bc2f
DustinKLo <dustinlo.pcc@gmail.com>
2019-12-07 14:34:49 -0800

    starting header bar changes

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   Route,
   // Link,
@@ -10,14 +10,30 @@ import Tosca from "./Tosca";
 import MetadataViewer from "./MetadataViewer";
 import ToscaOnDemand from "../pages/ToscaOnDemand";
 
+const style = {
+  // width: "100%",
+  background: "#4387ff",
+  color: "#ffff",
+  display: "flex",
+  padding: 10
+};
+
 export default function Routes(props) {
   let router = (
-    <Router>
-      <Route exact path="/" render={() => <Redirect to="/tosca" />} />
-      <Route exact path="/tosca" component={Tosca} />
-      <Route exact path="/tosca/metadata" component={MetadataViewer} />
-      <Route exact path="/tosca/on-demand" component={ToscaOnDemand} />
-    </Router>
+    <Fragment>
+      <div style={style}>
+        <div style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>Tosca</div>
+        <div style={{ flex: 0, paddingLeft: 5, paddingRight: 5 }}>stuff</div>
+        <div style={{ flex: 0, paddingLeft: 5, paddingRight: 5 }}>stuff</div>
+        <div style={{ flex: 0, paddingLeft: 5, paddingRight: 5 }}>stuff</div>
+      </div>
+      <Router>
+        <Route exact path="/" render={() => <Redirect to="/tosca" />} />
+        <Route exact path="/tosca" component={Tosca} />
+        <Route exact path="/tosca/metadata" component={MetadataViewer} />
+        <Route exact path="/tosca/on-demand" component={ToscaOnDemand} />
+      </Router>
+    </Fragment>
   );
   return router;
 }

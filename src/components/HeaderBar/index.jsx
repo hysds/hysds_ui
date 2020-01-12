f654dff3cfde6827fd539acc79a93094148f418d
DustinKLo <dustinlo.pcc@gmail.com>
2020-01-11 19:25:37 -0800

    convert from css to scss
    added dark mode for tosca main page (TODO: still need to do on demand and user rules)

--- src/components/HeaderBar/index.jsx
+++ src/components/HeaderBar/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+import { editTheme } from "../../redux/actions";
+
+import { Button } from "../Buttons";
 import { Link } from "react-router-dom";
 
-import "./style.css";
+import "./style.scss";
 
 const HeaderLink = props => {
   const { title, href, active } = props;
@@ -28,14 +33,25 @@ const HeaderTitle = props => {
 
 const HeaderBar = props => {
   const title = props.title || "HySDS";
+
+  const _themeHandler = () => {
+    const { darkMode } = props;
+    props.editTheme(!darkMode);
+    localStorage.setItem("dark-mode", !darkMode);
+  };
+
   return (
     <div className="header-bar">
       <ul className="header-bar-link-wrapper">
         <HeaderTitle title={title} />
         <HeaderLink to="/tosca" title="Tosca" active={1} />
         <HeaderLink title="Figaro" />
-        <HeaderLink style={{ flex: 1 }} />
-
+        <Button
+          label={props.darkMode ? "Light Mode" : "Dark Mode"}
+          onClick={_themeHandler}
+        />
+        <div className="header-bar-buffer"></div>
+        {/* <button>button</button> */}
         <li>
           <a>Logout</a>
         </li>
@@ -44,4 +60,12 @@ const HeaderBar = props => {
   );
 };
 
-export default HeaderBar;
+const mapStateToProps = state => ({
+  darkMode: state.themeReducer.darkMode
+});
+
+const mapDispatchToProps = dispatch => ({
+  editTheme: darkMode => dispatch(editTheme(darkMode))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderBar);

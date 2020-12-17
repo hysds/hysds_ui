b24bea1027124c86ba45c6f033dcdef3347ae9bc
dustinklo <dustinlo.pcc@gmail.com>
2020-12-17 09:49:54 -0800

    added metadata preview to tosca data viewer
    added metadata.state to facet and return field
    removed track_number from config/tosca.js

--- src/components/ToscaDataViewer/index.jsx
+++ src/components/ToscaDataViewer/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux"; // redux
 
 import { Link } from "react-router-dom";
+import ReactJson from "react-json-view";
 
 import { clickQueryRegion } from "../../redux/actions";
 import UserTags from "../UserTags";
@@ -10,22 +11,28 @@ import { Button } from "../Buttons";
 
 import { GRQ_REST_API_V1 } from "../../config";
 
+import { darkthemealt, lightthemebg } from "../../scss/constants.scss";
 import "./style.scss";
 
 const ToscaDataViewer = (props) => {
   const { res } = props;
+  const [viewData, setViewData] = useState(false);
+
   const endpoint = `${GRQ_REST_API_V1}/grq/user-tags`;
 
   let userTags =
     res.metadata && res.metadata.user_tags ? res.metadata.user_tags : [];
 
+  const metadataTheme = props.darkMode ? "monokai" : "rjv-default";
+  const backgroundColor = props.darkMode ? darkthemealt : lightthemebg;
+
+  let browseUrl = null;
+  if (res.urls) browseUrl = res.urls.find((url) => url.startsWith("http"));
+
   const clickQueryRegion = () => {
     const bbox = JSON.stringify(res.location.coordinates[0]);
     props.clickQueryRegion(bbox);
   };
-
-  let browseUrl = null;
-  if (res.urls) browseUrl = res.urls.find((url) => url.startsWith("http"));
 
   return (
     <div key={`${res._index}-${res._id}`} className="tosca-data-viewer">
@@ -42,23 +49,44 @@ const ToscaDataViewer = (props) => {
         index={res._index}
         id={res._id}
       />
-      <a href={browseUrl} />
       {browseUrl ? (
-        <a href={browseUrl} target="_blank">
+        <a className="tosca-browse-link" href={browseUrl} target="_blank">
           Browse
         </a>
       ) : null}
       <span />
+      <a
+        href="javascript:void(0)"
+        className="tosca-metadata-link"
+        onClick={() => setViewData(!viewData)}
+      >
+        Preview Metadata
+      </a>
       <Link
         className="tosca-metadata-link"
         to={`/tosca/metadata/${res._index}/${res._id}`}
         target="_none"
       >
-        Metadata
+        Full Metadata
       </Link>
+      {viewData ? (
+        <div className="tosca-metadata-preview">
+          <ReactJson
+            src={res}
+            displayDataTypes={false}
+            theme={metadataTheme}
+            style={{ backgroundColor }}
+            displayObjectSize={false}
+          />
+        </div>
+      ) : null}
     </div>
   );
 };
+
+const mapStateToProps = (state) => ({
+  darkMode: state.themeReducer.darkMode,
+});
 
 // Redux actions
 const mapDispatchToProps = (dispatch) => ({
@@ -69,4 +97,4 @@ ToscaDataViewer.propTypes = {
   res: PropTypes.object.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(ToscaDataViewer);
+export default connect(mapStateToProps, mapDispatchToProps)(ToscaDataViewer);

614f86d7f40ffef890d8289d7ef28f6ffeee320c
dustinklo <dustinlo.pcc@gmail.com>
2020-12-16 21:11:47 -0800

    added loading spinner when loading
    added error message when unable to retrieve metadata from elasticsearch
    TODO: need to add state_config to facet sidebar in tosca

--- src/pages/MetadataViewer/index.jsx
+++ src/pages/MetadataViewer/index.jsx
@@ -3,11 +3,12 @@ import { Helmet } from "react-helmet";
 import { connect } from "react-redux";
 
 import ReactJson from "react-json-view";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import HeaderBar from "../../components/HeaderBar";
 
 import { GRQ_ES_URL } from "../../config";
-
 import { darkthemebg, lightthemebg } from "../../scss/constants.scss";
 import "./style.scss";
 
@@ -15,18 +16,62 @@ const MetadataViewer = (props) => {
   const { id, index } = props.match.params;
   const classTheme = props.darkMode ? "__theme-dark" : "__theme-light";
 
-  const endpoint = `${GRQ_ES_URL}/${index}/_doc/${id}`;
   const [metadata, setMetadata] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const endpoint = `${GRQ_ES_URL}/${index}/_doc/${id}`;
 
   useEffect(() => {
     fetch(endpoint)
-      .then((res) => res.json())
-      .then((data) => setMetadata(data))
-      .catch((err) => console.error(err));
+      .then((res) => {
+        try {
+          return res.json();
+        } catch (err) {
+          throw new Error("Error retrieving metadata from Elasticsearch");
+        }
+      })
+      .then((data) => {
+        setLoading(false);
+        setMetadata(data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(true);
+        console.error(err);
+      });
   }, []);
 
   const theme = props.darkMode ? "monokai" : "rjv-default";
   const backgroundColor = props.darkMode ? darkthemebg : lightthemebg;
+
+  let view = null;
+  if (error) {
+    view = (
+      <div class="metadata-view-error">
+        <span>Error connecting to Elasticsearch</span>
+      </div>
+    );
+  } else if (loading) {
+    view = (
+      <div class="metadata-view-loader">
+        <span className="metadata-view-buffer">
+          <FontAwesomeIcon icon={faSpinner} spin={true} />
+        </span>
+        <span>Loading...</span>
+      </div>
+    );
+  } else {
+    view = (
+      <ReactJson
+        src={metadata}
+        displayDataTypes={false}
+        theme={theme}
+        style={{ backgroundColor: backgroundColor }}
+        displayObjectSize={false}
+      />
+    );
+  }
 
   return (
     <Fragment>
@@ -36,15 +81,7 @@ const MetadataViewer = (props) => {
       <HeaderBar title="GRQ - Metadata" theme={classTheme} />
 
       <div className={classTheme}>
-        <div className="metadata-view-container">
-          <ReactJson
-            src={metadata}
-            displayDataTypes={false}
-            theme={theme}
-            style={{ backgroundColor: backgroundColor }}
-            displayObjectSize={false}
-          />
-        </div>
+        <div className="metadata-view-container">{view}</div>
       </div>
     </Fragment>
   );

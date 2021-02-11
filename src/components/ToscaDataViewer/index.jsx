701b5f0ef98264331404f26b6f10b8bbc7503fdf
dustinklo <dustinlo.pcc@gmail.com>
2021-02-10 17:35:24 -0800

    added query region only if location.type is polygon

--- src/components/ToscaDataViewer/index.jsx
+++ src/components/ToscaDataViewer/index.jsx
@@ -67,7 +67,9 @@ const ToscaDataViewer = (props) => {
       >
         Full Metadata
       </Link>
-      {res.location && res.location.coordinates ? (
+      {res.location &&
+      res.location.coordinates &&
+      (res.location.type === "polygon" || res.location.type === "Polygon") ? (
         <Button size="small" label="Query Region" onClick={clickQueryRegion} />
       ) : null}
       {viewData ? (

97ebd331b1d6024871767e923041f9a0985acbbc
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2026-02-18 14:19:34 -0800

    updates based on testing

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /__tests__/],
         use: {
           loader: "babel-loader",
         },
